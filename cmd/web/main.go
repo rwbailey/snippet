efbb90ce25afa8cf1c37b11ef02e755e5acf215b
Richard Bailey <richard@richardbailey.xyz>
2022-05-05 23:20:54 +0100

    html templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,11 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
